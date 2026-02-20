f6567804198ce7d3c43c22d8c3cf2e264a6e17d7
JiTe365 <jarmo@jitece.nl>
2026-02-20 09:19:16 +0100

    env changed

--- tests/saucedemo-login.spec.ts
+++ tests/saucedemo-login.spec.ts
@@ -4,16 +4,10 @@ import { SauceDemoUsers } from '../utils/test-data';
 test('login with test data helper', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
-  const username = process.env.TEST_USERNAME ?? SauceDemoUsers.standard.username;
-  const password = process.env.TEST_PASSWORD ?? SauceDemoUsers.standard.password;
-
-  await page.locator('#user-name').fill(username);
-  await page.locator('#password').fill(password);
+  await page.locator('#user-name').fill(SauceDemoUsers.standard.username);
+  await page.locator('#password').fill(SauceDemoUsers.standard.password);
   await page.locator('#login-button').click();
 
-  // No baseURL dependency:
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-  // or robust alternative:
-  // await expect(page).toHaveURL(/\/inventory\.html$/);
+await expect(page).toHaveURL(/\/inventory\.html$/);
 });
 
